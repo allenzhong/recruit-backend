09e0a99199ae516fda08320eabc178daca0765f3
allenzhong <zhong.allen@gmail.com>
2020-09-09 21:22:22 +1200

    Add test for attributes on register method

--- CardApplication/CardApplication.Test/Controllers/CardControllerTests.cs
+++ CardApplication/CardApplication.Test/Controllers/CardControllerTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CardApplication.Application.Handlers;
@@ -5,6 +7,7 @@ using CardApplication.Application.Models;
 using CardApplication.Controllers;
 using CardApplication.Exceptions;
 using CardApplication.Test.Application.Models;
+using Castle.Core.Internal;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -57,5 +60,34 @@ namespace CardApplication.Test.Controllers
 
             Assert.IsType<ConflictResult>(result);
         }
+        
+        [Fact]
+        public void ShouldHaveRequiredAttributes_OnRegister()
+        {
+            var controllerType = _controller.GetType();
+            var methodInfo = controllerType.GetMethod("Register");
+
+            Assert.NotNull(methodInfo.GetAttribute<HttpPostAttribute>());
+        }
+        
+        [Theory]
+        [InlineData("Register", null, "register")]
+        public void ShouldHaveRouteAttributes_OnMethods(string methodName, Type[] types, string expectedTemplate)
+        {
+            AssertRouteTemplate<CardController>(methodName, types, expectedTemplate);
+        }
+        
+        private void AssertRouteTemplate<T>(string methodName, Type[] methodParameterTypes,
+            string expectedTemplate)
+        {
+            var controllerType = typeof(T);
+            var method = methodParameterTypes != null && methodParameterTypes.Any() ?
+                controllerType.GetMethod(methodName, methodParameterTypes)
+                : controllerType.GetMethod(methodName);
+            var routeAttribute = method.GetAttribute<RouteAttribute>();
+
+            Assert.True(routeAttribute.Template == expectedTemplate);
+        }
+
     }
 }
