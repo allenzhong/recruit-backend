8509b1c03e50471440044bbf31387fdb0e684b59
allenzhong <zhong.allen@gmail.com>
2020-09-09 16:40:16 +1200

    Add more tests for card validator

--- CardApplication/CardApplication.Test/CardModelValidatorTests.cs
+++ CardApplication/CardApplication.Test/CardModelValidatorTests.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 using Bogus;
 using CardApplication.Models;
 using Xunit;
+using ValidationResult = FluentValidation.Results.ValidationResult;
 
 namespace CardApplication.Test
 {
@@ -10,15 +12,138 @@ namespace CardApplication.Test
         [Fact]
         public void ShouldReturnValidWhenCardInfoIsValid()
         {
-            var card = new Faker<Card>()
+            var card = CreateValidFaker().Generate();
+            
+            var result = ValidateCard(card);
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ShouldReturnInvalidWhenIdIsNull()
+        {
+            var card = CreateValidFaker() 
+                .RuleFor(u => u.Id, f => Guid.Empty)
+                .Generate();
+            var result = ValidateCard(card);
+            Assert.False(result.IsValid);
+        }
+        
+        [Fact]
+        public void ShouldReturnInvalidWhenNameIsNull()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.Name, (f) => null)
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.False(result.IsValid);
+        }
+        
+        [Fact]
+        public void ShouldReturnInvalidWhenLenghtOfNameGreaterThan50()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.Name, (f) => f.Random.String(51))
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.False(result.IsValid);
+        }
+        
+        [Fact]
+        public void ShouldReturnInvalidWhenCreditCarNumberIsInvalid()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.CardNumber, (f) => f.Random.String(11))
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.False(result.IsValid);
+        }
+        
+        [Fact]
+        public void ShouldReturnValidWhenCvcLengthIs3()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.Cvc, (f) => string.Join("", f.Random.Digits(3)))
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.True(result.IsValid);
+        }
+        [Fact]
+        public void ShouldReturnValidWhenCvcLengthIs4()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.Cvc, (f) => string.Join("", f.Random.Digits(4)))
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void ShouldReturnInvalidWhenCvcIsInvalid()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.Cvc, (f) => f.Random.String(3))
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.False(result.IsValid);
+        }
+        
+        
+        [Fact]
+        public void ShouldReturnInvalidWhenCvcLengthGreaterThan4()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.Cvc, (f) => f.Random.String(5))
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void ShouldReturnInvalidWhenExpiryDateIsBeforeToday()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.ExpiryDate, (f) => f.Date.Past())
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.False(result.IsValid); 
+        }
+        
+        [Fact]
+        public void ShouldReturnValidWhenExpiryDateIsToday()
+        {
+            var card = CreateValidFaker()
+                .RuleFor(u => u.ExpiryDate, (f) => DateTime.Today)
+                .Generate();
+            
+            var result = ValidateCard(card);
+            Assert.True(result.IsValid); 
+        }
+        
+        private static Faker<Card> CreateValidFaker()
+        {
+            var faker = new Faker<Card>()
+                .RuleFor(u => u.Id, f => Guid.NewGuid())
                 .RuleFor(u => u.Name, (f) => f.Name.FullName())
                 .RuleFor(u => u.CardNumber, f => f.Finance.CreditCardNumber())
                 .RuleFor(u => u.Cvc, f => f.Finance.CreditCardCvv())
-                .RuleFor(u => u.ExpiryDate, f => f.Date.Between(DateTime.Today, DateTime.Today.AddYears(3)))
-                .Generate();
-            var cardValidator = new CardModelValidator();
-            var result = cardValidator.Validate(card); 
-            Assert.True(result.IsValid);
+                .RuleFor(u => u.ExpiryDate, f => f.Date.Between(DateTime.Today, DateTime.Today.AddYears(3)));
+            return faker;
         }
+
+        private static ValidationResult ValidateCard(Card card)
+        {
+            var cardValidator = new CardModelValidator();
+            var result = cardValidator.Validate(card);
+            return result;
+        }
+
     }
 }
--- CardApplication/CardApplication/Models/CardModelValidator.cs
+++ CardApplication/CardApplication/Models/CardModelValidator.cs
@@ -7,7 +7,7 @@ namespace CardApplication.Models
     {
         public CardModelValidator()
         {
-            RuleFor(c => c.Id).NotNull();
+            RuleFor(c => c.Id).NotEmpty().NotNull();
             RuleFor(c => c.CardNumber).CreditCard();
             RuleFor(c => c.Cvc).Matches(@"^[0-9]{3,4}$");
             RuleFor(c => c.Name).NotNull().MaximumLength(50);
